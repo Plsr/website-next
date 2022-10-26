6ebe1df7965f63af02d1c1464c6cfa0794686be2
Plsr <christianpoplawski@gmail.com>
2022-10-26 23:58:29 +0200

    Add basic dynamic routing for posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,42 @@
 import { useState, useEffect } from "react";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
 
-export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>()
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }: any) {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>();
+  console.log(allPostsData);
 
   useEffect(() => {
-    setIsDarkMode(document.documentElement.classList.contains('dark'))
-  }, [])
+    setIsDarkMode(document.documentElement.classList.contains("dark"));
+  }, []);
 
   const handleDarkModeToggleClick = () => {
-    document.documentElement.classList.toggle("dark")
-    localStorage.theme = isDarkMode ? 'light' : 'dark'
-    setIsDarkMode(!isDarkMode)
+    document.documentElement.classList.toggle("dark");
+    localStorage.theme = isDarkMode ? "light" : "dark";
+    setIsDarkMode(!isDarkMode);
   };
 
   return (
     <>
       <div>Here be home</div>
       <button onClick={handleDarkModeToggleClick}>Toggle dark mode</button>
+      <h2>All posts</h2>
+      <ul>
+        {allPostsData.map((postData) => (
+          <li key={postData.id}>
+            <Link href={`/posts/${postData.id}`}>{postData.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,38 @@
+import { getAllPostIds, getPostData, PostData } from "../../lib/posts";
+import { GetStaticPropsContext } from "next";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  const postData = await getPostData(params!.id as string);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }: props) {
+  return (
+    <>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </>
+  );
+}
+
+type props = {
+  postData: PostData;
+};
