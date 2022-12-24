12a7dc41d1aae3a01ef31fe47916e091e27ec68a
Plsr <christianpoplawski@gmail.com>
2022-12-24 17:15:30 +0100

    Move PostsList to distinct component

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,7 @@
 import { NextPageContext } from 'next'
-import Link from 'next/link'
-import { Button } from '../../components/button'
-import Divider from '../../components/divider'
 import { Pagination } from '../../components/pagination'
 import { PostData, getPaginatedPosts } from '../../lib/posts'
-import { StyledArticleContent } from '../../components/styled-article-content'
-import { TagsList } from '../../components/tags-list'
+import { PostsList } from '../../components/posts-list'
 
 export async function getServerSideProps(context: NextPageContext) {
   const page = Number(context.query.page) || 0
@@ -39,33 +35,5 @@ export default function PostsIndex({
         recordName="posts"
       />
     </>
-  )
-}
-
-type PostsListProps = {
-  posts: PostData[]
-}
-
-const PostsList = ({ posts }: PostsListProps) => {
-  return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id} className="mb-24">
-          <Link href={`/posts/${post.id}`}>
-            <div className="flex flex-col mb-8 ">
-              <span className="mb-2 text-sm text-slate-500">
-                {post.formattedDate}
-              </span>
-              <span className="text-3xl font-headline">{post.title}</span>
-            </div>
-          </Link>
-          <StyledArticleContent contentHtml={post.contentHtml} />
-          {post.tags && (
-            <TagsList className="mt-12" tags={post.tags?.split(' ')} />
-          )}
-          <Divider />
-        </li>
-      ))}
-    </ul>
   )
 }
