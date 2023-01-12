e4efa45220dfac9e8cd193b215ca829189278805
Plsr <christianpoplawski@gmail.com>
2023-01-12 22:27:58 +0100

    Make RSS feeds auto discoverable

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,18 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,700;1,400&family=Hind:wght@400;600&family=Josefin+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          href="/atom.xml"
+          title="RSS Feed"
+        />
+        <link
+          rel="alternate"
+          type="application/json"
+          href="/rss.json"
+          title="JSON Feed"
+        />
         <meta
           property="og:image"
           content="https://website-next-alpha.vercel.app/og.jpg"
