4103af855639e72970d73fffa3d33bcbf2f971a8
Christian Jarling <christianpoplawski@gmail.com>
2023-04-04 13:55:18 +0200

    Make latest post acutal links

--- components/recent-article.tsx
+++ components/recent-article.tsx
@@ -2,19 +2,16 @@ import formatDistance from 'date-fns/formatDistance'
 import { useRouter } from 'next/router'
 import { StyledArticleContent } from './styled-article-content'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 export default function RecentArticle({ date, id, title, excerpt }: props) {
   const postDate = Date.parse(date)
   const ago = formatDistance(postDate, new Date())
   const router = useRouter()
 
-  const onPostClick = () => {
-    router.push(`/post/${id}`)
-  }
-
   return (
-    <div
-      onClick={onPostClick}
+    <Link
+      href={`/post/${id}`}
       className="transition duration-300 cursor-pointer hover:bg-zinc-700 -ml-4 p-4 rounded-xl"
     >
       <small className="text-gray-500 mb-2 block">{ago} ago</small>
@@ -28,7 +25,7 @@ export default function RecentArticle({ date, id, title, excerpt }: props) {
       {excerpt && (
         <StyledArticleContent className="text-gray-400" contentHtml={excerpt} />
       )}
-    </div>
+    </Link>
   )
 }
 
