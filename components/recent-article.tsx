5c9ad5297ad51b43e4bdf1e728f8b9a80453a389
Christian Jarling <christianpoplawski@gmail.com>
2023-04-04 14:04:27 +0200

    Make latest post actual links (#35)

--- components/recent-article.tsx
+++ components/recent-article.tsx
@@ -1,21 +1,16 @@
 import formatDistance from 'date-fns/formatDistance'
-import { useRouter } from 'next/router'
 import { StyledArticleContent } from './styled-article-content'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 export default function RecentArticle({ date, id, title, excerpt }: props) {
   const postDate = Date.parse(date)
   const ago = formatDistance(postDate, new Date())
-  const router = useRouter()
-
-  const onPostClick = () => {
-    router.push(`/post/${id}`)
-  }
 
   return (
-    <div
-      onClick={onPostClick}
-      className="transition duration-300 cursor-pointer hover:bg-zinc-700 -ml-4 p-4 rounded-xl"
+    <Link
+      href={`/post/${id}`}
+      className="block transition duration-300 cursor-pointer hover:bg-zinc-700 -ml-4 p-4 rounded-xl"
     >
       <small className="text-gray-500 mb-2 block">{ago} ago</small>
       <h4
@@ -28,7 +23,7 @@ export default function RecentArticle({ date, id, title, excerpt }: props) {
       {excerpt && (
         <StyledArticleContent className="text-gray-400" contentHtml={excerpt} />
       )}
-    </div>
+    </Link>
   )
 }
 
