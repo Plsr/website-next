a099b6bf90c57d3ed20c10d66c66484f1f9f9ed0
Plsr <christianpoplawski@gmail.com>
2023-05-06 08:46:22 +0200

    Use valid html in social links

--- components/social-link.tsx
+++ components/social-link.tsx
@@ -8,10 +8,10 @@ type SocialLinkProps = {
 export const SocialLink = ({ children, ...linkProps }: SocialLinkProps) => {
   return (
     <a {...linkProps}>
-      <div className="hover:border-neutral-400 transition group flex flex-row items-center border border-zinc-500 rounded-lg py-2 px-4 text-xs text-zinc-300">
+      <span className="hover:border-neutral-400 transition group flex flex-row items-center border border-zinc-500 rounded-lg py-2 px-4 text-xs text-zinc-300">
         {children}
         <ArrowUpRightIcon className="transition group-hover:scale-110 w-3 h-3 ml-2" />
-      </div>
+      </span>
     </a>
   )
 }
