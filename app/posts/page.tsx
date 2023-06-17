54b79c189101aec466e6574ae7ea2b0a10c0a2d9
Plsr <christian@gigs.com>
2023-06-18 00:04:32 +0200

    Move posts index to app router

---
+++ app/posts/page.tsx
@@ -0,0 +1,59 @@
+import { PostsList } from '../../components/posts-list'
+
+import { BlogPost, getAllSortedEntries } from '../../lib/entries'
+import { getYear } from 'date-fns'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+
+export const metadata: Metadata = {
+  title: 'Posts - Chris Jarling',
+}
+
+type PostsByYear = {
+  [key: number]: BlogPost[]
+}
+
+const getPostsByYear = async () => {
+  const data = await getAllSortedEntries('posts')
+  const postsByYear: PostsByYear = {}
+
+  data.forEach((page) => {
+    const year = getYear(new Date(page.date))
+
+    if (!postsByYear[year]) {
+      postsByYear[year] = [page]
+      return
+    }
+    postsByYear[year].push(page)
+  })
+
+  return postsByYear
+}
+
+export default async function PostsIndex() {
+  const postsByYear = await getPostsByYear()
+
+  if (!postsByYear) {
+    notFound()
+  }
+
+  const sortedPostsByYears = Object.entries(postsByYear)
+    .sort((a, b) => parseInt(b[0]) - parseInt(a[0]))
+    .map((e) => e)
+
+  return (
+    <>
+      {sortedPostsByYears.map(([year, posts]) => (
+        <div className="mb-16" key={year}>
+          <div className="flex flex-row items-center mb-6">
+            <h2 className="font-headline text-xl text-neutral-300 mr-4">
+              {year}
+            </h2>
+            <div className="h-1 w-full border-b border-b-neutral-600" />
+          </div>
+          <PostsList posts={posts} />
+        </div>
+      ))}
+    </>
+  )
+}
