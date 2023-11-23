4fb2160472dba012ff193aae8a7b7f81feab7bc6
Plsr <christianpoplawski@gmail.com>
2023-11-23 12:13:01 +0100

    Update now page

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -16,12 +16,29 @@ const NowPage = () => {
       />
       <Paragraph>
         <ul className="mb-2 list-disc ml-4">
-          <li>I’m working full time for gigs</li>
-          <li>Reading Skin in the Game</li>
-          <li>Still reading a lot of blogs </li>
-          <li>Playing Pokemon Yellow Edition again after about 20 years</li>
+          <li>
+            I’m working full time for{' '}
+            <a className="text-rose-500" href="https://gigs.com">
+              gigs
+            </a>{' '}
+            (we're hiring 🤫)
+          </li>
+          <li>
+            Currently in Stay-at-home-Dad mode, spending a lot of time with the
+            family, awaiting our second child
+          </li>
+          <li>
+            Reading{' '}
+            <a
+              className="text-rose-500"
+              href="https://lesetagebu.ch/buch/before-the-coffee-gets-cold"
+            >
+              Before the Coffee Gets Cold
+            </a>
+          </li>
+          <li>Trying to get back in the habit of writing more on here</li>
         </ul>
-        <small>Last updated: 5th May, 2023</small>
+        <small>Last updated: 23rd November, 2023</small>
       </Paragraph>
     </div>
   )
