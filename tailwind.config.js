663c28d112b1dca905635495b5bf6f7a70a7bf65
Plsr <christian@gigs.com>
2023-12-12 14:41:51 +0100

    monospace! monospace! monospace!

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,8 +36,8 @@ module.exports = {
       },
     },
     fontFamily: {
-      title: ['sans-serif'],
-      body: ['sans-serif'],
+      title: ['monospace'],
+      body: ['monospace'],
     },
     extend: {},
   },
