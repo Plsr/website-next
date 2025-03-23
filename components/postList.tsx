d31b6e947ad03f0e5fdbdea9629cbc99425b41f9
Plsr <christian@gigs.com>
2025-03-23 07:29:33 +0100

    Add date to PostListItems

--- components/postList.tsx
+++ components/postList.tsx
@@ -1,5 +1,6 @@
 import { MDXContent } from '@content-collections/mdx/react'
 import { Post } from 'content-collections'
+import format from 'date-fns/format'
 
 type Props = {
   posts: Post[]
@@ -8,11 +9,20 @@ export const PostList = ({ posts }: Props) => {
   return (
     <>
       {posts.map((post) => (
-        <div className="dark:prose-invert prose" key={post.title}>
-          <h2>{post.title}</h2>
-          <MDXContent code={post.mdx} />
-        </div>
+        <PostListItem key={post.title} post={post} />
       ))}
     </>
+  )
+}
+
+const PostListItem = ({ post }: { post: Post }) => {
+  return (
+    <div className="dark:prose-invert prose">
+      <h2>{post.title}</h2>
+      <MDXContent code={post.mdx} />
+      <small className="text-base-600">
+        {format(new Date(post.date), 'MMMM d, yyyy')}
+      </small>
+    </div>
   )
 }
