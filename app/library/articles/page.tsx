91373049ae098c110bbbb0f64843abff4fe2b529
Plsr <christian@gigs.com>
2025-04-03 09:57:21 +0200

    Improve reading notes index screen

--- app/library/articles/page.tsx
+++ app/library/articles/page.tsx
@@ -16,25 +16,25 @@ const ArticlesIndexPage = () => {
   )
 
   return (
-    <div className="prose prose-invert">
-      <h1>Article reading notes</h1>
-      <p>Notes on articles I read and found interesting.</p>
-      <div className="mb-24 prose" />
+    <>
+      <div className="prose prose-invert mb-24">
+        <h1>Article reading notes</h1>
+        <p>Notes on articles I read and found interesting.</p>
+      </div>
+
       {sortedLibraryArticles.map((libraryArticle) => (
-        <div key={libraryArticle._id} className="mb-36">
-          <h2>
-            <a href={libraryArticle.link}>{libraryArticle.title}</a>
-          </h2>
-          <StyledArticleContent contentHtml={libraryArticle.body.html} />
-          <Link
-            href={`/library/articles/${libraryArticle.computedSlug}`}
-            className="mt-4 block"
-          >
+        <Link
+          href={`/library/articles/${libraryArticle.computedSlug}`}
+          key={libraryArticle._id}
+          className=" -ml-4 -mr-4 mb-2 py-2 px-4 rounded-md  flex flex-col md:flex-row md:items-center md:justify-between gap-x-6  hover:bg-base-900/50 hover:shadow-xs transition-all"
+        >
+          <h2 className="not-prose text-base-300">{libraryArticle.title}</h2>
+          <span className="shrink-0 text-base-600">
             {format(new Date(libraryArticle.createdAt), 'do LLL, yyyy')}
-          </Link>
-        </div>
+          </span>
+        </Link>
       ))}
-    </div>
+    </>
   )
 }
 
