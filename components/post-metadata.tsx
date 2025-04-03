c70129aad9fde6f3826bc44cd18f8fd99686faa0
Plsr <christian@gigs.com>
2025-04-03 07:41:48 +0200

    Adapt article content styling for light mode

--- components/post-metadata.tsx
+++ components/post-metadata.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
 export const PostMetadata = ({ children }: props) => {
-  return <small className="text-gray-500 text-sm">{children}</small>
+  return <small className="text-neutral-600 text-sm">{children}</small>
 }
 
 type props = {
