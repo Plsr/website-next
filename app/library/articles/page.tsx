1727384b61f390fdc1a9a178253a7dfd4ab8e75a
Christian Jarling <christianpoplawski@gmail.com>
2025-04-03 11:06:44 +0200

    Improve reading notes (#92)
    * Improve reading notes index screen
    
    * Show article metadate on reading notes

--- app/library/articles/page.tsx
+++ app/library/articles/page.tsx
@@ -1,40 +1,52 @@
-import { StyledArticleContent } from 'components/styled-article-content'
-import { compareDesc, format } from 'date-fns'
+import { getArticleMetadata } from 'data/getArticleMetadata'
+import { format } from 'date-fns'
 import { Metadata } from 'next'
 import Link from 'next/link'
-
-import { allLibraryArticles } from '.contentlayer/generated'
 
 export const metadata: Metadata = {
   title: 'Library: Articles - Chris Jarling',
   description: 'A list of links I read and found interesting.',
 }
 
-const ArticlesIndexPage = () => {
-  const sortedLibraryArticles = allLibraryArticles.sort((a, b) =>
-    compareDesc(new Date(a.createdAt), new Date(b.createdAt)),
-  )
+const ArticlesIndexPage = async () => {
+  const articlesWithMetadata = await getArticleMetadata()
 
   return (
-    <div className="prose prose-invert">
-      <h1>Article reading notes</h1>
-      <p>Notes on articles I read and found interesting.</p>
-      <div className="mb-24 prose" />
-      {sortedLibraryArticles.map((libraryArticle) => (
-        <div key={libraryArticle._id} className="mb-36">
-          <h2>
-            <a href={libraryArticle.link}>{libraryArticle.title}</a>
-          </h2>
-          <StyledArticleContent contentHtml={libraryArticle.body.html} />
-          <Link
-            href={`/library/articles/${libraryArticle.computedSlug}`}
-            className="mt-4 block"
-          >
+    <>
+      <div className="prose prose-invert mb-24">
+        <h1>Article reading notes</h1>
+        <p>Notes on articles I read and found interesting.</p>
+      </div>
+
+      {articlesWithMetadata.map((libraryArticle) => (
+        <Link
+          href={`/library/articles/${libraryArticle.computedSlug}`}
+          key={libraryArticle._id}
+          className=" -ml-4 -mr-4 md:mb-2 mb-4 py-2 px-4 rounded-md  flex flex-col md:flex-row md:items-center md:justify-between gap-x-6  hover:bg-base-900/50 hover:shadow-xs transition-all"
+        >
+          <div>
+            <h2 className="not-prose text-base-300">{libraryArticle.title}</h2>
+            {(libraryArticle.faviconHref || libraryArticle.pageName) && (
+              <div className="flex items-center gap-x-2 mt-1 md:mb-0 mb-2">
+                {libraryArticle.faviconHref && (
+                  <img
+                    src={libraryArticle.faviconHref}
+                    alt={libraryArticle.title}
+                    className="w-4 h-4"
+                  />
+                )}
+                <span className="text-xs text-base-600">
+                  {libraryArticle.pageName}
+                </span>
+              </div>
+            )}
+          </div>
+          <span className="shrink-0 text-base-600">
             {format(new Date(libraryArticle.createdAt), 'do LLL, yyyy')}
-          </Link>
-        </div>
+          </span>
+        </Link>
       ))}
-    </div>
+    </>
   )
 }
 
---
+++ data/getArticleMetadata.ts
@@ -0,0 +1,41 @@
+import { compareDesc } from 'date-fns'
+import { JSDOM } from 'jsdom'
+
+import { allLibraryArticles } from '.contentlayer/generated'
+
+export const getArticleMetadata = async () => {
+  const sortedLibraryArticles = allLibraryArticles.sort((a, b) =>
+    compareDesc(new Date(a.createdAt), new Date(b.createdAt)),
+  )
+
+  const articlesWithMetadata = await Promise.all(
+    sortedLibraryArticles.map(async (article) => {
+      if (!article.link) {
+        return {
+          ...article,
+          pageName: null,
+          faviconHref: null,
+        }
+      }
+
+      const url = new URL(article.link)
+      const res = await fetch(article.link)
+      const data = await res.text()
+      const dom = new JSDOM(data)
+      const title = url.host.replace('www.', '')
+      const faviconElement = dom.window.document.querySelectorAll(
+        'link[rel="shortcut icon"],link[rel="icon"]',
+      )[0]
+
+      const faviconHref = (faviconElement as HTMLLinkElement)?.href
+
+      return {
+        ...article,
+        pageName: title,
+        faviconHref,
+      }
+    }),
+  )
+
+  return articlesWithMetadata
+}
