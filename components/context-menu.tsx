81f8d53e2c4c03d27f161cbee5ef9ae0a4d4d30b
Plsr <christian@gigs.com>
2025-05-29 09:12:21 +0200

    Add support for keyboard navigation

--- components/context-menu.tsx
+++ components/context-menu.tsx
@@ -1,21 +1,25 @@
 'use client'
 
+import clsx from 'clsx'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useState, useEffect } from 'react'
 import { RemoveScroll } from 'react-remove-scroll'
 
 const menuItems = [
-  { label: 'Home', href: '/' },
-  { label: 'Writing', href: '/posts' },
-  { label: 'Now', href: '/now' },
-  { label: 'About', href: '/about' },
-  { label: 'CV', href: '/cv' },
-  { label: 'Tags', href: '/tags' },
-  { label: 'Reading Notes', href: '/reading-notes' },
+  { label: 'Home', href: '/', id: 'home' },
+  { label: 'Writing', href: '/posts', id: 'writing' },
+  { label: 'Now', href: '/now', id: 'now' },
+  { label: 'About', href: '/about', id: 'about' },
+  { label: 'CV', href: '/cv', id: 'cv' },
+  { label: 'Tags', href: '/tags', id: 'tags' },
+  { label: 'Reading Notes', href: '/reading-notes', id: 'reading-notes' },
 ]
 
 export const ContextMenu = () => {
   const [showMenu, setShowMenu] = useState(false)
+  const [activeItem, setActiveItem] = useState<string | null>(menuItems[0].id)
+  const router = useRouter()
 
   useEffect(() => {
     const contextMenuHotkeyHandler = (event: KeyboardEvent) => {
@@ -28,6 +32,45 @@ export const ContextMenu = () => {
         event.preventDefault()
         setShowMenu(false)
       }
+
+      if (showMenu && event.key === 'ArrowDown') {
+        event.preventDefault()
+        const currentIndex = menuItems.findIndex(
+          (item) => item.id === activeItem,
+        )
+
+        const nextIndex = currentIndex + 1
+
+        if (nextIndex >= menuItems.length) {
+          return
+        }
+
+        setActiveItem(menuItems[nextIndex].id)
+      }
+
+      if (showMenu && event.key === 'ArrowUp') {
+        event.preventDefault()
+        const currentIndex = menuItems.findIndex(
+          (item) => item.id === activeItem,
+        )
+
+        const nextIndex = currentIndex - 1
+
+        if (nextIndex < 0) {
+          return
+        }
+
+        setActiveItem(menuItems[nextIndex].id)
+      }
+
+      if (showMenu && event.key === 'Enter') {
+        event.preventDefault()
+        const activeMenuItem = menuItems.find((item) => item.id === activeItem)
+        if (activeMenuItem) {
+          router.push(activeMenuItem.href)
+          debouncedSetShowMenu(false)
+        }
+      }
     }
 
     window.addEventListener('keydown', contextMenuHotkeyHandler)
@@ -35,7 +78,17 @@ export const ContextMenu = () => {
     return () => {
       window.removeEventListener('keydown', contextMenuHotkeyHandler)
     }
-  }, [showMenu])
+  }, [showMenu, activeItem])
+
+  const debouncedSetShowMenu = (visible: boolean) => {
+    setTimeout(() => {
+      setShowMenu(visible)
+    }, 200)
+  }
+
+  const handleMouseOver = (id: string) => {
+    setActiveItem(id)
+  }
 
   if (!showMenu) return null
 
@@ -48,8 +101,12 @@ export const ContextMenu = () => {
             <Link
               key={item.href}
               href={item.href}
-              className="block px-4 py-2 text-sm text-gray-200 hover:bg-base-800 rounded"
-              onClick={() => setShowMenu(false)}
+              className={clsx(
+                'block px-4 py-2 text-sm text-gray-200 rounded',
+                activeItem === item.id && 'bg-base-800',
+              )}
+              onClick={() => debouncedSetShowMenu(false)}
+              onMouseOver={() => handleMouseOver(item.id)}
             >
               {item.label}
             </Link>
